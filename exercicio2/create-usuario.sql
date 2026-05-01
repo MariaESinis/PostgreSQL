df0442a33618da31c23f5a2db57812cab20ccd6a
Maria Eduarda Sinis <130075460+MariaESinis@users.noreply.github.com>
2026-05-01 15:14:12 -0300

    feat: add table usuario;

--- exercicio2/create-usuario.sql
+++ exercicio2/create-usuario.sql
@@ -1,1 +1,12 @@
 CREATE SCHEMA IF NOT EXIST exercicio2;
+
+CREATE TABLE IF NOT EXIST usuario(
+    nome VARCHAR(150),
+    email VARCHAR(320),
+    senha VARCHAR(20),
+    telefone INTEGER,
+    status-usuario VARCHAR(8),
+    date-criacao DATE,
+    idade INTEGER,
+    nivel-acesso INTEGER
+)
