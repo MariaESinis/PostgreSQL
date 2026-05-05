488f30043cc6cd8672b008fc80bb3100c174981c
Maria Eduarda Sinis <130075460+MariaESinis@users.noreply.github.com>
2026-05-05 19:38:19 -0300

    fix: fix SQL syntax

--- exercicio2/create-usuario.sql
+++ exercicio2/create-usuario.sql
@@ -1,58 +1,60 @@
 CREATE SCHEMA IF NOT EXISTS exercicio2;
 
 --Funcao valida nome
-CREATE OR REPLACE FUNCTION exercicio2.fn_valida_nome(nome TEXT) 
+CREATE OR REPLACE FUNCTION exercicio2.fn_valida_nome(input_nome TEXT) 
 RETURNS BOOLEAN AS $$
 BEGIN
     RETURN (
         length(trim(nome)) >= 3        
-        AND nome ~ '^[A-Za-zÀ-ÿ][A-Za-zÀ-ÿ ]*[A-Za-zÀ-ÿ]$' 
-        AND nome !~ '  '                        
-        AND nome ~ '^[A-Za-zÀ-ÿ ]+$'            
+        AND         input_nome ~         '^[A-Za-zÀ-ÿ][A-Za-zÀ-ÿ ]*[A-Za-zÀ-ÿ]$' 
+        AND         input_nome !~        '  '                        
+    
     );
 END;
 $$ LANGUAGE plpgsql;
 
 --Funcao valida telefone
-CREATE OR REPLACE FUNCTION exercicio2.fn_valida_telefone(tel TEXT) 
+CREATE OR REPLACE FUNCTION exercicio2.fn_valida_telefone(input_tel TEXT) 
 RETURNS BOOLEAN AS $$
 BEGIN
     RETURN (
-        tel ~ '^[0-9]{10,11}$'                  
-        AND (length(tel) = 10 OR (length(tel) = 11 AND substring(tel from 3 for 1) = '9'))
+                    input_tel ~         '^[0-9]{10,11}$'                  
+        AND         (length(tel) =      10 
+        OR          (length(tel) =      11 
+        AND         substring(tel from 3 for 1) = '9'))
+    
     );
 END;
 $$ LANGUAGE plpgsql;
 
---Funcao valida senha
-CREATE OR REPLACE FUNCTION exercicio2.fn_valida_senha(senha TEXT, email TEXT)
-RETURNS BOOLEAN AS $$
+--Funcao valida email
+CREATE OR REPLACE FUNCTION exercicio2.fn_valida_email(input_email TEXT)
+    RETURNS BOOLEAN AS $$
+    LANGUAGE plpgsql;
 BEGIN
         RETURN (
-        length(senha) >= 8                    
-        AND senha !~ ' '                      
-        AND senha ~ '[A-Z]'                   
-        AND senha ~ '[a-z]'                   
-        AND senha ~ '[0-9]'                
-        AND senha ~ '[!@#$%^&*]' 
-        AND senha <> email
+        input_email     LIKE           '_%@%._%' 
+        AND         input_email ~*     '^[A-Z0-9._%+-]{1,64}@[A-Z0-9.-]{1,255}\.[A-Z]{2,}$' 
+    
     );
 END;
-$$ LANGUAGE plpgsql;
+$$ 
     
 CREATE TABLE IF NOT EXISTS exercicio2.usuario(
-    nome VARCHAR(150),
-    email VARCHAR(320), 
-    senha VARCHAR(100),
-    telefone VARCHAR(11),
-    status VARCHAR(8) NOT NULL DEFAULT 'ativo',
-    data_criacao DATE NOT NULL DEFAULT CURRENT_DATE,
-    idade INTEGER CHECK (idade BETWEEN 14 AND 120),
-    nivel_acesso INTEGER CHECK (nivel_acesso BETWEEN 1 and 5),
+    nome                 VARCHAR(150)        NOT NULL,
+    email                VARCHAR(320)        NOT NULL, 
+    senha                VARCHAR(100)        NOT NULL,
+    telefone             VARCHAR(11)         NOT NULL,
+    status               VARCHAR(8)          NOT NULL         DEFAULT         'ativo',
+    data_criacao         DATE                NOT NULL         DEFAULT         CURRENT_TIMESTAMP,
+    idade                INTEGER             NOT NULL,
+    nivel_acesso         INTEGER             NOT NULL,
 
-    CONSTRAINT chk_nome_valido CHECK (exercicio2.fn_valida_nome(nome)),
-    CONSTRAINT chk_usuario_email CHECK (email LIKE '%@%.%'),
-    CONSTRAINT chk_senha_valido check(exercicio2.fn_valida_senha(senha, email)),
-    CONSTRAINT chk_usuario_telefone CHECK (exercicio2.fn_valida_telefone(telefone)),
-    CONSTRAINT chk_usuario_status CHECK (status IN ('ativo', 'inativo', 'bloqueado', 'pendente'))
+    CONSTRAINT     chk_nome_valido              CHECK     (exercicio2.fn_valida_nome(nome)),
+    CONSTRAINT     chk_usuario_email            CHECK     (exercicio2.fn_validade_email(email)),
+    CONSTRAINT     chk_senha_valido             CHECK     (exercicio2.fn_valida_senha(),
+    CONSTRAINT     chk_usuario_telefone         CHECK     (exercicio2.fn_valida_telefone(telefone)),
+    CONSTRAINT     chk_usuario_status           CHECK     (status IN ('ativo', 'inativo', 'bloqueado', 'pendente'))
+    CONSTRAINT     chk_usuario_idade            CHECK     (idade BETWEEN 14 AND 120),
+    CONSTRAINT     chk_usuario_nivel_acesso     CHECK     (nivel_acesso BETWEEN 1 AND 5)
 );
