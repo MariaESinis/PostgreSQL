e33a48673196f71fb4eb6fdade96a98086f35009
Maria Eduarda Sinis <130075460+MariaESinis@users.noreply.github.com>
2026-05-09 14:24:52 -0300

    fix: fix SQL syntax

--- exercicio2/create-usuario.sql
+++ exercicio2/create-usuario.sql
@@ -1,11 +1,16 @@
-DROP FUNCTION IF EXISTS exercicio2.fn_valida_nome(text);
-DROP FUNCTION IF EXISTS exercicio2.fn_valida_telefone(text);
-DROP FUNCTION IF EXISTS exercicio2.fn_valida_email(text);
 CREATE SCHEMA IF NOT EXISTS exercicio2;
 
+DROP FUNCTION IF EXISTS exercicio2.fn_valida_nome(TEXT) CASCADE;
+DROP FUNCTION IF EXISTS exercicio2.fn_valida_telefone(TEXT) CASCADE;
+DROP FUNCTION IF EXISTS exercicio2.fn_valida_email(TEXT) CASCADE;
+
 -- Funcao valida nome
-CREATE OR REPLACE FUNCTION exercicio2.fn_valida_nome(input_nome TEXT) 
-RETURNS BOOLEAN AS $$
+
+CREATE OR REPLACE FUNCTION exercicio2.fn_valida_nome(input_nome TEXT)
+RETURNS BOOLEAN 
+LANGUAGE plpgsql
+IMMUTABLE
+AS $$
 BEGIN
     RETURN (
         length(trim(input_nome)) >= 3        
@@ -13,11 +18,14 @@ BEGIN
         AND input_nome !~ '  ' -- Evita espaços duplos
     );
 END;
-$$ LANGUAGE plpgsql;
+$$;
 
 -- Funcao valida telefone
 CREATE OR REPLACE FUNCTION exercicio2.fn_valida_telefone(input_tel TEXT) 
-RETURNS BOOLEAN AS $$
+RETURNS BOOLEAN 
+LANGUAGE plpgsql
+IMMUTABLE
+AS $$
 BEGIN
     RETURN (
         input_tel ~ '^[0-9]{10,11}$'                  
@@ -25,37 +33,88 @@ BEGIN
             length(input_tel) = 10 
             OR (length(input_tel) = 11 AND substring(input_tel from 3 for 1) = '9')
         )
+        AND substring(input_tel from 1 for 2) IN (
+            '11', '12', '13', '14', '15', '16', '17', '18', '19', '21', '22', '24', '27', '28', '31', '32', '33', 
+            '34', '35', '37', '38', '41', '42', '43', '44', '45', '46', '47', '48', '49', '51', '53', '54', '55', 
+            '61', '62', '63', '64', '65', '66', '67', '68', '69', '71', '73', '74', '75', '77', '79', '81', '82', 
+            '83', '84', '85', '86', '87', '88', '89', '91', '92', '93', '94', '95', '96', '97', '98', '99'
+        )
     );
 END;
-$$ LANGUAGE plpgsql;
+$$;
 
 -- Funcao valida email
 CREATE OR REPLACE FUNCTION exercicio2.fn_valida_email(input_email TEXT)
-RETURNS BOOLEAN AS $$
+RETURNS BOOLEAN 
+LANGUAGE plpgsql
+IMMUTABLE
+AS $$
 BEGIN
     RETURN (
         input_email LIKE '_%@%._%' 
         AND input_email ~* '^[A-Z0-9._%+-]{1,64}@[A-Z0-9.-]{1,255}\.[A-Z]{2,}$' 
     );
 END;
-$$ LANGUAGE plpgsql;
-
+$$;
 -- Tabela Usuario
 CREATE TABLE IF NOT EXISTS exercicio2.usuario(
+    
+    id INTEGER GENERATED ALWAYS AS IDENTITY (
+    START WITH 1000
+    INCREMENT BY 1 
+    MINVALUE 1 
+    MAXVALUE 1000 
+    CACHE 1 
+    SEQUENCE NAME exercicio2.seq_usuario_id),
+    
     nome            VARCHAR(150)        NOT NULL,
     email           VARCHAR(320)        NOT NULL, 
     senha           VARCHAR(100)        NOT NULL,
     telefone        VARCHAR(11)         NOT NULL,
-    status          VARCHAR(10)         NOT NULL         DEFAULT 'ativo',
-    data_criacao    TIMESTAMP           NOT NULL         DEFAULT CURRENT_TIMESTAMP,
-    idade           INTEGER             NOT NULL,
-    nivel_acesso    INTEGER             NOT NULL,
+    status          VARCHAR(10)         NOT NULL    DEFAULT 'ativo',
+    data_criacao    TIMESTAMPTZ         NOT NULL    DEFAULT CURRENT_TIMESTAMP,
+    idade           SMALLINT            NOT NULL,
+    nivel_acesso    CHAR                NOT NULL,
 
-    CONSTRAINT chk_nome_valido           CHECK (exercicio2.fn_valida_nome(nome)),
-    CONSTRAINT chk_usuario_email         CHECK (exercicio2.fn_valida_email(email)),
-    CONSTRAINT chk_senha_valido          CHECK (senha ~ '[A-Z]' AND senha ~ '[a-z]' AND senha ~ '[0-9]' AND senha ~ '[!@#$%^&*()]' AND LENGTH(senha) >= 8),
-    CONSTRAINT chk_usuario_telefone      CHECK (exercicio2.fn_valida_telefone(telefone)),
-    CONSTRAINT chk_usuario_status        CHECK (status IN ('ativo', 'inativo', 'bloqueado', 'pendente')),
-    CONSTRAINT chk_usuario_idade         CHECK (idade BETWEEN 14 AND 120),
-    CONSTRAINT chk_usuario_nivel_acesso  CHECK (nivel_acesso BETWEEN 1 AND 5)
+    CONSTRAINT pk_usuario_id PRIMARY KEY (id),
+
+    CONSTRAINT chk_usuario_nome          
+    CHECK (
+        exercicio2.fn_valida_nome(nome)
+    ),
+
+    CONSTRAINT usuario_email UNIQUE (email),  
+    CONSTRAINT chk_usuario_email         
+    CHECK (
+        exercicio2.fn_valida_email(email)
+    ),
+    
+    CONSTRAINT chk_usuario_senha        
+    CHECK (
+        senha ~ '[A-Z]' 
+        AND senha ~ '[a-z]' 
+        AND senha ~ '[0-9]' 
+        AND senha ~ '[!@#$%^&*()]' 
+        AND LENGTH(senha) >= 8),
+
+    CONSTRAINT usuario_telefone UNIQUE (telefone),
+    CONSTRAINT chk_usuario_telefone      
+    CHECK (
+        exercicio2.fn_valida_telefone(telefone)
+    ),
+    
+    CONSTRAINT chk_usuario_status        
+    CHECK (
+        status IN ('ativo', 'inativo', 'bloqueado', 'pendente')
+    ),
+    
+    CONSTRAINT chk_usuario_idade         
+    CHECK (
+        idade BETWEEN 14 AND 120
+    ),
+    
+    CONSTRAINT chk_usuario_nivel_acesso  
+    CHECK (
+        nivel_acesso IN('1', '2', '3', '4', '5')
+    )
 );
