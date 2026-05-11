d3e6583a9cd26aa439eb4c61a5c7f5ef782beb40
Maria Eduarda Sinis <130075460+MariaESinis@users.noreply.github.com>
2026-05-11 20:53:01 -0300

    feat: create table categorias

---
+++ exercicio3/create-catogorias.sql
@@ -0,0 +1,34 @@
+CREATE SCHEMA IF NOT EXISTS exercicio3;
+--CREATE TYPE exercicio3.status AS ENUM ('ativo', 'inativo', 'bloqueado', 'arquivada');
+
+--Funcao valida nome
+CREATE OR REPLACE FUNCTION exercicio3.fn_valida_nome(IN p_nome TEXT)
+RETURNS BOOLEAN 
+LANGUAGE plpgsql
+IMMUTABLE
+AS $$
+BEGIN
+    RETURN (
+        length(trim(p_nome)) >= 3        
+        AND p_nome ~ '^[A-Za-zÀ-ÿ][A-Za-zÀ-ÿ ]*[A-Za-zÀ-ÿ]$' 
+        AND p_nome !~ '  ' -- Evita espaços duplos
+    );
+END;
+$$;
+
+CREATE TABLE IF NOT EXISTS exercicio3.categorias(
+    id INTEGER	GENERATED ALWAYS AS IDENTITY(
+        START WITH 1
+        INCREMENT BY 1
+        MINVALUE 1
+        MAXVALUE 1000
+        CACHE 1
+        SEQUENCE NAME exercicio3.seq_categorias_id
+    ),
+    nome          VARCHAR(150)           NOT NULL,
+    status        exercicio3.status      NOT NULL,
+    data_criacao  TIMESTAMPTZ            NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT pk_categorias_id PRIMARY KEY(id)
+);
+
