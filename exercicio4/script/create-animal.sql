d9e0b3c527bc951fe675b93cf18b54a9e969da11
Maria Eduarda Sinis <130075460+MariaESinis@users.noreply.github.com>
2026-05-14 19:12:12 -0300

    feat: add constraints, fix syntax,

--- exercicio4/script/create-animal.sql
+++ exercicio4/script/create-animal.sql
@@ -4,7 +4,7 @@ CREATE TYPE exercicio4.status_animal AS ENUM(
     'obito'
 );
 
-CREATE TABLE IF NO EXISTS exercicio4.animal(
+CREATE TABLE IF NOT EXISTS exercicio4.animal(
     id INTEGER GENERATED ALWAYS AS IDENTITY(
         START WITH 0
         INCREMENT BY 1
@@ -12,7 +12,7 @@ CREATE TABLE IF NO EXISTS exercicio4.animal(
         MAXVALUE 100000
         CACHE 1
         SEQUENCE NAME exercicio3.seq_animais_id
-    )
+    ),
     nome                    VARCHAR(150)                    NULL,
     data_nascimento         TIMESTAMPTZ                     NULL,
     peso                    DECIMAL(6,3)                    NOT NULL,
@@ -20,5 +20,21 @@ CREATE TABLE IF NO EXISTS exercicio4.animal(
     situacao                exercicio4.status_animal        NOT NULL,
     created_at              TIMESTAMPTZ                     NOT NULL,
     updated_at              TIMESTAMPTZ                     NOT NULL,
+    tutor_id                INTEGER                         NULL             REFERENCES exercicio4.tutor(id),
+    raca_id                 INTEGER                         NOT NULL         REFERENCES exercicio4.raca(id),
+
+    CONSTRAINT pk_animal_id PRIMARY KEY(id),
+    CONSTRAINT chk_animal_nome CHECK(
+        exercicio4.fn_animal_nome(nome)
+    ),
+
+    CONSTRAINT chk_animal_peso CHECK(
+        peso > 0
+    ),
+    
+    CONSTRAINT chk_animal_sexo CHECK (
+		sexo IN('F', 'M', 'D', 'H')
+	)
+    
     
 )
