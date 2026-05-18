ca841bdc5b04c802972348b4a2b85a439e7c5c5a
Maria Eduarda Sinis <130075460+MariaESinis@users.noreply.github.com>
2026-05-18 20:08:32 -0300

    cleanup: select is

--- tests/exercicio5/fn_nome.test.sql
+++ tests/exercicio5/fn_nome.test.sql
@@ -1,19 +1,19 @@
 BEGIN;
 
-SET search_path to ex_pgtap;
-SELECT plan(6);
+SET search_path TO ex_pgtap;
+
+SELECT plan(5);
 
 SELECT diag('Retorna TRUE se o nome for válido');
 SELECT is(ex5_helpers.fn_nome('Maria Eduarda Barboza Sinis'), true, 'Nome válido');
 
-SELECT diag('Nome com simbolos deve ser invalido');
+SELECT diag('Nome com símbolos deve ser inválido');
 SELECT is(ex5_helpers.fn_nome('M@ri@ Edu@rda Barboza Sinis'), false, 'Nome inválido');
 SELECT is(ex5_helpers.fn_nome('Maria.Eduarda Barboza.Sinis'), false, 'Nome inválido');
 SELECT is(ex5_helpers.fn_nome('Maria Eduarda Barboza S!n!s'), false, 'Nome inválido');
-SELECT is(ex5_helpers.fn_nome('Maria Eduarda Barboza Sinis'), false, 'Nome inválido');
 
-SELECT diag('Nome com espaços duplos deve ser invalido');
-SELECT is(ex5_helpers.fn_nome('Maria  Eduarda  Barboza Sinis'), false, 'Nome invalido');
+SELECT diag('Nome com espaços duplos deve ser inválido');
+SELECT is(ex5_helpers.fn_nome('Maria  Eduarda  Barboza Sinis'), false, 'Nome inválido');
 
 SELECT * FROM finish();
 
