1f0dd43cfa68f393e61368fd4936b2fea5be9627
Maria Eduarda Sinis <130075460+MariaESinis@users.noreply.github.com>
2026-05-18 15:25:14 -0300

    feat: create table equipamento

---
+++ exercicio5/script/create-equipamento.sql
@@ -0,0 +1,35 @@
+CREATE TYPE ex5_ti.equipamento_status AS ENUM(
+    'disponivel',
+    'alocado',
+    'manutencao',
+    'baixado'
+);
+
+CREATE TABLE IF NOT EXISTS ex5_tiequipamentos(
+    id  INTEGER GENERATED ALWAYS AS IDENTITY(
+        START WITH 0
+        INCREMENT BY 1
+        MINVALUE 0
+        MAXVALUE 100000000
+        CACHE 1
+        SEQUENCE NAME seq_equipamento_id
+    ),
+    num_patrimonial         INTEGER                         NOT NULL,
+    num_serie               VARCHAR(10)                     NOT NULL,
+    fabricante              VARCHAR(100)                    NOT NULL,
+    modelo                  VARCHAR(150)                    NOT NULL,
+    data_aquisicao          TIMESTAMPTZ                     NOT NULL,
+    valor_aquisicao         DECIMAL(11,2)                   NOT NULL,
+    situacao                ex5_ti.equipamento_status       NOT NULL,
+    created_at              TIMESTAMPTZ                     NOT NULL,
+    updated_at              TIMESTAMPTZ                     NOT NULL,
+
+    CONSTRAINT pk_equipamento_id PRIMARY KEY(id),
+
+    CONSTRAINT uq_equipamento_num_patrimonial UNIQUE(num_patrimonial),
+    CONSTRAINT uq_equipamento_num_serie UNIQUE(num_serie),
+
+    CONSTRAINT chk_equipamento_valor_aquisicao CHECK(
+        valor_aquisicao > 0
+    )
+)
