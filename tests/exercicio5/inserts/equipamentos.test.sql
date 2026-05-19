fb46016ee4d10271822464d77af04179818c75e1
Maria Eduarda Sinis <130075460+MariaESinis@users.noreply.github.com>
2026-05-19 20:10:01 -0300

    test: add test for uq_equipamento_num_patrimonial

--- tests/exercicio5/inserts/equipamentos.test.sql
+++ tests/exercicio5/inserts/equipamentos.test.sql
@@ -89,6 +89,35 @@ SELECT throws_ok(
      '23514',
     'new row for relation "equipamentos" violates check constraint "chk_equipamento_fabricante"'
 );
+
+SELECT throws_ok(
+    $$
+      INSERT INTO ex5_ti.equipamentos(
+        num_patrimonial, 
+        num_serie, 
+        fabricante, 
+        modelo, 
+        data_aquisicao, 
+        valor_aquisicao, 
+        situacao, 
+        created_at, 
+        updated_at
+      )
+	    VALUES (
+        3005, 
+        'A356', 
+        'Samsung',
+        'BR404', 
+        '2025-04-03 09:00:00-03', 
+        500.00, 
+        'disponivel', 
+        '2025-04-03 12:00:00-03', 
+        '2025-04-06 00:00:00-03'
+      );
+    $$,
+     '23505',
+    'new row for relation "equipamentos" violates check constraint "uq_equipamento_num_patrimonial"'
+);
 SELECT * FROM finish();
 
 ROLLBACK;
