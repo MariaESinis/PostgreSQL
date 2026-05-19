b8e331988b8a598de09c2e9e36ea4a6be601cedf
Maria Eduarda Sinis <130075460+MariaESinis@users.noreply.github.com>
2026-05-19 19:48:35 -0300

    fix: REGEX

--- exercicio5/script/create-functions.sql
+++ exercicio5/script/create-functions.sql
@@ -52,9 +52,8 @@ LANGUAGE plpgsql
 AS $$
 BEGIN
     RETURN (
-        trim(p_nome) ~ '^[A-Za-zÀ-ÿ]+( [A-Za-zÀ-ÿ]+)*$'
-        AND length(trim(p_nome)) >= 3
-        AND p_nome !~ ' '
+        p_nome ~ '^[A-Za-z0-9]+$'
+        AND length(p_nome) >= 3
     );
 END;
 $$;
