1f6445cc3c533230d4f1f1e9be33bcf928f96f3e
Maria Eduarda Sinis <130075460+MariaESinis@users.noreply.github.com>
2026-05-19 20:26:45 -0300

    fix: syntax

--- tests/exercicio5/inserts/equipamentos.test.sql
+++ tests/exercicio5/inserts/equipamentos.test.sql
@@ -1,126 +1,153 @@
 BEGIN;
 
 SET search_path TO ex_pgtap;
-SELECT plan(5);
 
-SELECT throws_ok(
-    $$ 
-      INSERT INTO ex5_ti.equipamentos(
-        num_patrimonial, 
-        num_serie, 
-        fabricante, 
-        modelo, 
-        data_aquisicao, 
-        valor_aquisicao, 
-        situacao, 
-        created_at, 
-        updated_at
-      )
-	    VALUES (
-        3004,
-        'A23BG', 
-        'Lazer', 
-        'A32', 
-        '2024-04-02 14:00:00-03',
-        -45.00, 
-        'disponivel', 
-        '2024-04-03 00:00:00-03', 
-        '2024-04-03 00:00:00-03'
-      );
-    $$,
-     '23514',
-    'new row for relation "equipamentos" violates check constraint "chk_equipamento_valor_aquisicao"'
-);
+SELECT plan(4);
 
+-- 1) CHECK valor_aquisicao
 SELECT throws_ok(
     $$
-      INSERT INTO ex5_ti.equipamentos(
-        num_patrimonial, 
-        num_serie, 
-        fabricante, 
-        modelo, 
-        data_aquisicao, 
-        valor_aquisicao, 
-        situacao, 
-        created_at, 
+    INSERT INTO ex5_ti.equipamentos(
+        num_patrimonial,
+        num_serie,
+        fabricante,
+        modelo,
+        data_aquisicao,
+        valor_aquisicao,
+        situacao,
+        created_at,
         updated_at
-      )
-	    VALUES (
-        3005, 
-        'A356!%', 
-        'Rayzor',
-        'BR404', 
-        '2025-04-03 09:00:00-03', 
-        500.00, 
-        'disponivel', 
-        '2025-04-03 12:00:00-03', 
-        '2025-04-06 00:00:00-03'
-      );
+    )
+    VALUES (
+        3001,
+        'ABC123',
+        'Dell',
+        'X1',
+        '2024-04-02 14:00:00-03',
+        -45.00,
+        'disponivel',
+        '2024-04-03 00:00:00-03',
+        '2024-04-03 00:00:00-03'
+    );
     $$,
-     '23514',
+    '23514',
+    'new row for relation "equipamentos" violates check constraint "chk_equipamento_valor_aquisicao"'
+
+);
+
+-- 2) CHECK num_serie
+SELECT throws_ok(
+    $$
+    INSERT INTO ex5_ti.equipamentos(
+        num_patrimonial,
+        num_serie,
+        fabricante,
+        modelo,
+        data_aquisicao,
+        valor_aquisicao,
+        situacao,
+        created_at,
+        updated_at
+    )
+    VALUES (
+        3002,
+        'A35!@%',   -- inválido
+        'Dell',
+        'X1',
+        '2024-04-02 14:00:00-03',
+        100.00,
+        'disponivel',
+        '2024-04-03 00:00:00-03',
+        '2024-04-03 00:00:00-03'
+    );
+    $$,
+    '23514',
     'new row for relation "equipamentos" violates check constraint "chk_equipamento_num_serie"'
 );
 
+-- 3) CHECK fabricante
 SELECT throws_ok(
     $$
-      INSERT INTO ex5_ti.equipamentos(
-        num_patrimonial, 
-        num_serie, 
-        fabricante, 
-        modelo, 
-        data_aquisicao, 
-        valor_aquisicao, 
-        situacao, 
-        created_at, 
+    INSERT INTO ex5_ti.equipamentos(
+        num_patrimonial,
+        num_serie,
+        fabricante,
+        modelo,
+        data_aquisicao,
+        valor_aquisicao,
+        situacao,
+        created_at,
         updated_at
-      )
-	    VALUES (
-        3005, 
-        'A356', 
-        '@pple',
-        'BR404', 
-        '2025-04-03 09:00:00-03', 
-        500.00, 
-        'disponivel', 
-        '2025-04-03 12:00:00-03', 
-        '2025-04-06 00:00:00-03'
-      );
+    )
+    VALUES (
+        3003,
+        'ABC999',
+        '@pple',  -- inválido
+        'X1',
+        '2024-04-02 14:00:00-03',
+        100.00,
+        'disponivel',
+        '2024-04-03 00:00:00-03',
+        '2024-04-03 00:00:00-03'
+    );
     $$,
-     '23514',
+    '23514',
     'new row for relation "equipamentos" violates check constraint "chk_equipamento_fabricante"'
 );
 
-INSERT INTO ex5_ti.equipamentos(num_patrimonial, num_serie)
-VALUES (3005, 'ABC123');
+-- 4) UNIQUE num_patrimonial
+INSERT INTO ex5_ti.equipamentos(
+    num_patrimonial,
+    num_serie,
+    fabricante,
+    modelo,
+    data_aquisicao,
+    valor_aquisicao,
+    situacao,
+    created_at,
+    updated_at
+)
+VALUES (
+    3004,
+    'ABC111',
+    'Dell',
+    'X1',
+    '2024-04-02 14:00:00-03',
+    100.00,
+    'disponivel',
+    '2024-04-03 00:00:00-03',
+    '2024-04-03 00:00:00-03'
+);
 
 SELECT throws_ok(
     $$
-      INSERT INTO ex5_ti.equipamentos(
-        num_patrimonial, 
-        num_serie, 
-        fabricante, 
-        modelo, 
-        data_aquisicao, 
-        valor_aquisicao, 
-        situacao, 
-        created_at, 
+    INSERT INTO ex5_ti.equipamentos(
+        num_patrimonial,
+        num_serie,
+        fabricante,
+        modelo,
+        data_aquisicao,
+        valor_aquisicao,
+        situacao,
+        created_at,
         updated_at
-      )
-	    VALUES (
-        3005, 
-        'A356', 
-        'Samsung',
-        'BR404', 
-        '2025-04-03 09:00:00-03', 
-        500.00, 
-        'disponivel', 
-        '2025-04-03 12:00:00-03', 
-        '2025-04-06 00:00:00-03'
-      );
+    )
+    VALUES (
+        3004,  -- duplicado
+        'ABC222',
+        'Dell',
+        'X2',
+        '2024-04-02 14:00:00-03',
+        200.00,
+        'disponivel',
+        '2024-04-03 00:00:00-03',
+        '2024-04-03 00:00:00-03'
+    );
     $$,
-     '23505',
+    '23505',
     'new row for relation "equipamentos" violates unique constraint "uq_equipamento_num_patrimonial"'
 );
+
 SELECT * FROM finish();
 
 ROLLBACK;
