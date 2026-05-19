635e69f5bdea04e433c3d9f6698a2f9415575561
Maria Eduarda Sinis <130075460+MariaESinis@users.noreply.github.com>
2026-05-19 19:27:21 -0300

    fix: syntax for number of error

--- tests/exercicio5/inserts/equipamentos.test.sql
+++ tests/exercicio5/inserts/equipamentos.test.sql
@@ -57,7 +57,7 @@ SELECT throws_ok(
         '2025-04-06 00:00:00-03'
       )
     $$
-     '23514'
+     '23514',
     'new row for relation "equipamentos" violates check constraint "chk_equipamento_num_serie"'
 );
 
