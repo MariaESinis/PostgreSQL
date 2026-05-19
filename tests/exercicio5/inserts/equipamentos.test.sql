c3ba98404c87d604dc087dcf4b725e2a6af167d8
Maria Eduarda Sinis <130075460+MariaESinis@users.noreply.github.com>
2026-05-19 19:18:38 -0300

    test: add test for check num_serie

--- tests/exercicio5/inserts/equipamentos.test.sql
+++ tests/exercicio5/inserts/equipamentos.test.sql
@@ -30,6 +30,33 @@ SELECT throws_ok(
     $$,
      '23514',
     'new row for relation "equipamentos" violates check constraint "chk_equipamento_valor_aquisicao"'
+
+    $$
+      INSERT INTO ex5_ti.equipamentos(
+        num_patrimonial, 
+        num_serie, 
+        fabricante, 
+        modelo, 
+        data_aquisicao, 
+        valor_aquisicao, 
+        situacao, 
+        created_at, 
+        updated_at
+      )
+	    VALUES (
+        3005, 
+        'A356!%', 
+        'Rayzor'
+        'BR404', 
+        '2025-04-03 00:00:00-03', 
+        500.00, 
+        'disponivel', 
+        '2025-04-03 00:00:00-03', 
+        '2025-04-06 00:00:00-03'
+      )
+    $$
+     '23514'
+    'new row for relation "equipamentos" violates check constraint "chk_equipamento_num_serie"'
 );
 
 SELECT * FROM finish();
