f6cf58dbc86aacc8e1013b048bf6f2e1a4208c43
Maria Eduarda Sinis <130075460+MariaESinis@users.noreply.github.com>
2026-05-19 20:02:37 -0300

    test: add test for constraint chk_equipamento_fabricante

--- tests/exercicio5/inserts/equipamentos.test.sql
+++ tests/exercicio5/inserts/equipamentos.test.sql
@@ -1,7 +1,7 @@
 BEGIN;
 
 SET search_path TO ex_pgtap;
-SELECT plan(2);
+SELECT plan(3);
 
 SELECT throws_ok(
     $$ 
@@ -61,6 +61,34 @@ SELECT throws_ok(
     'new row for relation "equipamentos" violates check constraint "chk_equipamento_num_serie"'
 );
 
+SELECT throws_ok(
+    $$
+      INSERT INTO ex5_ti.equipamentos(
+        num_patrimonial, 
+        num_serie, 
+        fabricante, 
+        modelo, 
+        data_aquisicao, 
+        valor_aquisicao, 
+        situacao, 
+        created_at, 
+        updated_at
+      )
+	    VALUES (
+        3005, 
+        'A356', 
+        '@pple',
+        'BR404', 
+        '2025-04-03 09:00:00-03', 
+        500.00, 
+        'disponivel', 
+        '2025-04-03 12:00:00-03', 
+        '2025-04-06 00:00:00-03'
+      );
+    $$,
+     '23514',
+    'new row for relation "equipamentos" violates check constraint "chk_equipamento_fabricante"'
+);
 SELECT * FROM finish();
 
 ROLLBACK;
