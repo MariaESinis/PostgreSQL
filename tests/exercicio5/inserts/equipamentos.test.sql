656526e5d1cb0a2f28c0900b8499fb09d1cc4d05
Maria Eduarda Sinis <130075460+MariaESinis@users.noreply.github.com>
2026-05-19 19:44:48 -0300

    fix: syntax

--- tests/exercicio5/inserts/equipamentos.test.sql
+++ tests/exercicio5/inserts/equipamentos.test.sql
@@ -26,7 +26,7 @@ SELECT throws_ok(
         'disponivel', 
         '2024-04-03 00:00:00-03', 
         '2024-04-03 00:00:00-03'
-      )
+      );
     $$,
      '23514',
     'new row for relation "equipamentos" violates check constraint "chk_equipamento_valor_aquisicao"'
@@ -55,7 +55,7 @@ SELECT throws_ok(
         'disponivel', 
         '2025-04-03 12:00:00-03', 
         '2025-04-06 00:00:00-03'
-      )
+      );
     $$,
      '23514',
     'new row for relation "equipamentos" violates check constraint "chk_equipamento_num_serie"'
