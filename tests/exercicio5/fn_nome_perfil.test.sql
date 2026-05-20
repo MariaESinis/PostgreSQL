a02e845e8d4c0a6c2ec26fb59591722f6ad7e57e
Maria Eduarda Sinis <130075460+MariaESinis@users.noreply.github.com>
2026-05-20 12:20:55 -0300

    test: add select

--- tests/exercicio5/fn_nome_perfil.test.sql
+++ tests/exercicio5/fn_nome_perfil.test.sql
@@ -6,7 +6,14 @@ SELECT plan(1); --Adicionar um numero antes de rodar o teste
 SELECT diag('Retorna TRUE se o nome de perfil for válido');
 SELECT is(ex5_helpers.fn_nome_perfil('User123'), TRUE, 'Nome de perfil válido');
 
+SELECT diag('Retorna FALSE se o nome de perfil tiver simbolo');
+SELECT is(ex5_helpers.fn_nome_perfil('User!23'), FALSE, 'Nome de perfil inválido');
 
+SELECT diag('Retorna FALSE se o nome de perfil tiver espaço');
+SELECT is(ex5_helpers.fn_nome_perfil('User 123'), FALSE, 'Nome de perfil inválido');
+
+SELECT diag('Retorna FALSE se o nome de perfil tiver caracter < 3');
+SELECT is(ex5_helpers.fn_nome_perfil('U12'), FALSE, 'Nome de perfil inválido');
 
 SELECT * FROM finish();
 ROLLBACK;
