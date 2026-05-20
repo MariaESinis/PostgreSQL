30b71f70fdd24db6d13c3c09fa7f1b32399899e2
Maria Eduarda Sinis <130075460+MariaESinis@users.noreply.github.com>
2026-05-20 12:22:42 -0300

    fix: number for plan

--- tests/exercicio5/fn_nome_perfil.test.sql
+++ tests/exercicio5/fn_nome_perfil.test.sql
@@ -1,7 +1,7 @@
 BEGIN;
 
 SET search_path TO ex_pgtap;
-SELECT plan(1); --Adicionar um numero antes de rodar o teste
+SELECT plan(4); --Adicionar um numero antes de rodar o teste
 
 SELECT diag('Retorna TRUE se o nome de perfil for válido');
 SELECT is(ex5_helpers.fn_nome_perfil('User123'), TRUE, 'Nome de perfil válido');
