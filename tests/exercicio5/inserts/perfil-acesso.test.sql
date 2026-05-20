7dc4fd8dac20d8217409e90b3662270641ec686e
Maria Eduarda Sinis <130075460+MariaESinis@users.noreply.github.com>
2026-05-20 12:51:30 -0300

    test: add test for chk_perfil_acesso_nome

--- tests/exercicio5/inserts/perfil-acesso.test.sql
+++ tests/exercicio5/inserts/perfil-acesso.test.sql
@@ -1,7 +1,7 @@
 BEGIN;
 
 SET search_path TO ex_pgtap;
-SELECT plan(1); --Adicionar um numero antes de rodar o teste
+SELECT plan(2); --Adicionar um numero antes de rodar o teste
 
 SELECT throws_ok(
     $$
@@ -22,6 +22,24 @@ SELECT throws_ok(
     'new row for relation "perfil_acesso" violates check constraint "chk_perfil_acesso_nivel_acesso"'
 );
 
+SELECT throws_ok(
+    $$
+      INSERT INTO ex5_ti.perfil_acesso(
+	      nome, 
+        nivel_acesso, 
+        descricao, 
+        is_active
+      )
+	    VALUES (
+        'User 1@3', 
+        '1', 
+        'Uso corporativo', 
+        'ativo'
+      );
+    $$,
+    '23514',
+    'new row for relation "perfil_acesso" violates check constraint "chk_perfil_acesso_nome"'
+);
 SELECT * FROM finish();
 
 ROLLBACK;
