7f4648e732f1bb61e34d55e64d7f8578d23ce8ae
Maria Eduarda Sinis <130075460+MariaESinis@users.noreply.github.com>
2026-05-20 12:09:24 -0300

    test: create test for table perfil-acesso

---
+++ tests/exercicio5/inserts/perfil-acesso.test.sql
@@ -0,0 +1,28 @@
+BEGIN;
+
+SET search_path TO ex_pgtap;
+SELECT plan(); --Adicionar um numero antes de rodar o teste
+
+SELECT throws_ok(
+    $$
+      INSERT INTO ex5_ti.perfil_acesso(
+	      nome, 
+        nivel_acesso, 
+        descricao, 
+        is_active
+      )
+	    VALUES (
+        'User123', 
+        '1', 
+        'Uso corporativo', 
+        'ativo'
+      );
+    $$,
+    '23514',
+    'new row for relation "perfil_acesso" violates check constraint "chk_perfil_acesso"'
+
+
+)
+
+SELECT * FROM finish();
+ROLLBACK;
