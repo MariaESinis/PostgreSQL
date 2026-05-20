70f0c24f5aadd5e8c9bc05e33bb6672fb0395379
Maria Eduarda Sinis <130075460+MariaESinis@users.noreply.github.com>
2026-05-20 11:36:25 -0300

    test: fn_num_serie

---
+++ tests/exercicio5/fn_num_serie.test.sql
@@ -0,0 +1,20 @@
+BEGIN;
+
+SET search_path TO ex_pgtap;
+SELECT plan();
+
+SELECT diag('Retorna TRUE se o numero de serie for válido');
+SELECT is(ex5_helpers.fn_num_serie('ABC123'), TRUE, 'Número de serie válido');
+
+SELECT diag('Numero de serie com espaço deve ser inválido');
+SELECT is(ex5_helpers.fn_num_serie('DEF 123'), FALSE, 'Número de serie inválido');
+
+SELECT diag('Numero de serie com simbolo deve ser inválido');
+SELECT is(ex5_helpers.fn_num_serie('GHI!@#'), FALSE, 'Número de serie inválido');
+
+SELECT diag('Numero de serie com caracter < 3 deve ser inválido');
+SELECT is(ex5_helpers.fn_num_serie('J1'), FALSE, 'Número de serie inválido');
+
+SELECT * FROM finish();
+
+ROLLBACK;
