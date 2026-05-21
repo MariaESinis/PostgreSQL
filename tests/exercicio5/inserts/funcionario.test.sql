7a95f13bf733e0dac68d08a48c2663755f76309e
Maria Eduarda Sinis <130075460+MariaESinis@users.noreply.github.com>
2026-05-21 16:38:24 -0300

    fix: change order of inserts

--- tests/exercicio5/inserts/funcionario.test.sql
+++ tests/exercicio5/inserts/funcionario.test.sql
@@ -3,23 +3,7 @@ BEGIN;
 SET search_path TO ex_pgtap;
 SELECT plan(2);
 
-	INSERT INTO ex5_rh.departamento(
-    nome,
-    sigla,
-    orcamento_mensal,
-    data_criacao,
-    gestor_id
-	)
-VALUES (
-    'Tecnologia da Informação',
-    'TI',
-    75000.00,
-    '2026-05-20 10:00:00-03',
-    1
-    
-	);
-
-INSERT INTO ex5_rh.funcionario(
+	INSERT INTO ex5_rh.funcionario(
     matricula,
     nome,
     pis_pasep,
@@ -46,76 +30,93 @@ INSERT INTO ex5_rh.funcionario(
     CURRENT_TIMESTAMP,
     1
 	);
+	
+	INSERT INTO ex5_rh.departamento(
+    nome,
+    sigla,
+    orcamento_mensal,
+    data_criacao,
+    gestor_id
+	)
+	VALUES (
+    'Tecnologia da Informação',
+    'TI',
+    75000.00,
+    '2026-05-20 10:00:00-03',
+    1
+  );
 
-SELECT throws_ok(
+	
+	SELECT throws_ok(
 	$$
-	INSERT INTO ex5_rh.funcionario
-	(
-  	matricula,
-  	nome,
-  	pis_pasep,
-  	email,
-  	telefone,
-  	cargo,
-  	salario_base,
-  	status,
-  	created_at,
-  	updated_at,
-  	departamento_id
-  )
-	VALUES 
-	(
-		1002,
-		'M@r!na Lim$',
-		'12345678901',
-		'Marina.Lima@email.com',
-		'11999999999',
-		'Desenvolvedor back-end',
-		7500.00,
-		'ativo',
-		CURRENT_TIMESTAMP,
-		CURRENT_TIMESTAMP,
-		1
-	);
-	$$,
-	'23514',
-  'new row for relation "funcionario" violates check constraint "chk_funcionario_nome"'
+		INSERT INTO ex5_rh.funcionario
+		(
+			matricula,
+			nome,
+			pis_pasep,
+			email,
+			telefone,
+			cargo,
+			salario_base,
+			status,
+			created_at,
+			updated_at,
+			departamento_id
+		)
+		VALUES 
+		(
+			1002,
+			'M@r!na Lim$',
+			'12345678901',
+			'Marina.Lima@email.com',
+			'11999999999',
+			'Desenvolvedor back-end',
+			7500.00,
+			'ativo',
+			CURRENT_TIMESTAMP,
+			CURRENT_TIMESTAMP,
+			1
+		);
+		$$,
+		'23514',
+		'new row for relation "funcionario" violates check constraint "chk_funcionario_nome"'
 );
 	
-SELECT throws_ok(
-	$$
-	INSERT INTO ex5_rh.funcionario
-	(
-  	matricula,
-  	nome,
-  	pis_pasep,
-  	email,
-  	telefone,
-  	cargo,
-  	salario_base,
-  	status,
-  	created_at,
-  	updated_at,
-  	departamento_id
-  )
-	VALUES 
-	(
-		1002,
-		'Marina Lima',
-		'12345678901',
-		'marialimaemail.com',
-		'11999999999',
-		'Desenvolvedor back-end',
-		7500.00,
-		'ativo',
-		CURRENT_TIMESTAMP,
-		CURRENT_TIMESTAMP,
-		1
-	);
-	$$,
-	'23514',
-  'new row for relation "funcionario" violates check constraint "chk_funcionario_email"'
+	SELECT throws_ok(
+		$$
+			INSERT INTO ex5_rh.funcionario
+			(
+				matricula,
+				nome,
+				pis_pasep,
+				email,
+				telefone,
+				cargo,
+				salario_base,
+				status,
+				created_at,
+				updated_at,
+				departamento_id
+			)
+			VALUES 
+			(
+				1002,
+				'Marina Lima',
+				'12345678901',
+				'marialimaemail.com',
+				'11999999999',
+				'Desenvolvedor back-end',
+				10500.00,
+				'ativo',
+				CURRENT_TIMESTAMP,
+				CURRENT_TIMESTAMP,
+				1
+			);
+			$$,
+			'23514',
+			'new row for relation "funcionario" violates check constraint "chk_funcionario_email"'
 );
+
 SELECT * FROM finish();
 
 ROLLBACK;
