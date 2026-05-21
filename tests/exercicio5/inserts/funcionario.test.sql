1d8eb3c072fd34ed0122f3ea2a4104f584c0a1f8
Maria Eduarda Sinis <130075460+MariaESinis@users.noreply.github.com>
2026-05-21 16:30:48 -0300

    test: create test for chk email

--- tests/exercicio5/inserts/funcionario.test.sql
+++ tests/exercicio5/inserts/funcionario.test.sql
@@ -68,7 +68,7 @@ SELECT throws_ok(
 		1002,
 		'M@r!na Lim$',
 		'12345678901',
-		'joao.silva@email.com',
+		'Marina.Lima@email.com',
 		'11999999999',
 		'Desenvolvedor back-end',
 		7500.00,
@@ -82,7 +82,40 @@ SELECT throws_ok(
   'new row for relation "funcionario" violates check constraint "chk_funcionario_nome"'
 );
 	
-
+SELECT throws_ok(
+	$$
+	INSERT INTO ex5_rh.funcionario
+	(
+  	matricula,
+  	nome,
+  	pis_pasep,
+  	email,
+  	telefone,
+  	cargo,
+  	salario_base,
+  	status,
+  	created_at,
+  	updated_at,
+  	departamento_id
+  )
+	VALUES 
+	(
+		1002,
+		'Marina Lima',
+		'12345678901',
+		'marialimaemail.com',
+		'11999999999',
+		'Desenvolvedor back-end',
+		7500.00,
+		'ativo',
+		CURRENT_TIMESTAMP,
+		CURRENT_TIMESTAMP,
+		1
+	);
+	$$,
+	'23514',
+  'new row for relation "funcionario" violates check constraint "chk_funcionario_email"'
+);
 SELECT * FROM finish();
 
 ROLLBACK;
