2fc2f9afaeebdcf6ab11f4128d62bef77c4de5bf
Maria Eduarda Sinis <130075460+MariaESinis@users.noreply.github.com>
2026-05-25 10:40:22 -0300

    test: sigla invalida

--- tests/exercicio5/inserts/departamento.teste.sql
+++ tests/exercicio5/inserts/departamento.teste.sql
@@ -2,7 +2,7 @@ BEGIN;
 
 SET search_path TO ex_pgtap;
 
-SELECT plan(1);
+SELECT plan(2);
 
 SET CONSTRAINTS ex5_rh.fk_departamento_id, ex5_rh.fk_gestor_id DEFERRED;
 
@@ -72,6 +72,27 @@ $$,
 'new row for relation "departamento" violates check constraint "chk_departamento_nome"'
 );
 
+--TESTE 2: sigla invalida
+SELECT throws_ok(
+    $$
+        INSERT INTO ex5_rh.departamento (
+            nome,
+            sigla,
+            orcamento_mensal,
+            data_criacao,
+            gestor_id
+        )
+        VALUES (
+            'Tecnologia da Informação',
+            'T',
+            75000.00,
+            '2026-05-20 10:00:00-03',
+            1
+        );
+    $$,
+    '23514',
+    'new row for relation "departamento" violates check constraint "chk_departamento_nome"'
+)
 SELECT * FROM finish();
 
 ROLLBACK;
