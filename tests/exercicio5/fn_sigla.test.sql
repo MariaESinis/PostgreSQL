521982cd6212d105234d3e016800a50b20858dbe
Maria Eduarda Sinis <130075460+MariaESinis@users.noreply.github.com>
2026-05-25 11:11:01 -0300

    feat: add test for number of caracter invalid

--- tests/exercicio5/fn_sigla.test.sql
+++ tests/exercicio5/fn_sigla.test.sql
@@ -9,6 +9,8 @@ SELECT is(ex5_helpers.fn_sigla('B2'), TRUE, 'Sigla válida');
 SELECT diag('Sigla com simbolo deve ser inválida');
 SELECT is(ex5_helpers.fn_sigla('B!C'), FALSE, 'Sigla inválida');
 
+SELECT diag('Sigla com caracter < 2 deve ser inválida');
+SELECT is(ex5_helpers.fn_sigla('B'), FALSE, 'Sigla inválida');
 SELECT * FROM finish();
 
 ROLLBACK;
