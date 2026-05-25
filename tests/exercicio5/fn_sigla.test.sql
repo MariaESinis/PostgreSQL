4f67577311b573fc26915003243b0b36ae090fea
Maria Eduarda Sinis <130075460+MariaESinis@users.noreply.github.com>
2026-05-25 11:08:44 -0300

    fix: registry for sigla symbol

--- tests/exercicio5/fn_sigla.test.sql
+++ tests/exercicio5/fn_sigla.test.sql
@@ -7,7 +7,7 @@ SELECT diag('Retorna TRUE se a sigla for válida');
 SELECT is(ex5_helpers.fn_sigla('B2'), TRUE, 'Sigla válida');
 
 SELECT diag('Sigla com simbolo deve ser inválida');
-SELECT is(ex5_helpers.fn_sigla('B!C'), TRUE, 'Sigla válida');
+SELECT is(ex5_helpers.fn_sigla('B!C'), FALSE, 'Sigla inválida');
 
 SELECT * FROM finish();
 
