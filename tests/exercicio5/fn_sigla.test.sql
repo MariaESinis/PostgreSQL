91ee77cacab3f75e700a8ddde4c91f592aa9a114
Maria Eduarda Sinis <130075460+MariaESinis@users.noreply.github.com>
2026-05-25 11:06:13 -0300

    test: fn_sigla

--- tests/exercicio5/fn_sigla.test.sql
+++ tests/exercicio5/fn_sigla.test.sql
@@ -6,6 +6,9 @@ SELECT plan(1);
 SELECT diag('Retorna TRUE se a sigla for válida');
 SELECT is(ex5_helpers.fn_sigla('B2'), TRUE, 'Sigla válida');
 
+SELECT diag('Sigla com simbolo deve ser inválida');
+SELECT is(ex5_helpers.fn_sigla('B!C'), TRUE, 'Sigla válida');
+
 SELECT * FROM finish();
 
 ROLLBACK;
