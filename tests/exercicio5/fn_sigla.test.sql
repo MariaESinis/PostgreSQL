8406d4defa8a3f3a4df483f2e923bb1f8ab8af55
Maria Eduarda Sinis <130075460+MariaESinis@users.noreply.github.com>
2026-05-25 11:07:23 -0300

    fix: number for plan

--- tests/exercicio5/fn_sigla.test.sql
+++ tests/exercicio5/fn_sigla.test.sql
@@ -1,7 +1,7 @@
 BEGIN;
 
 SET search_path TO ex_pgtap;
-SELECT plan(1);
+SELECT plan(2);
 
 SELECT diag('Retorna TRUE se a sigla for válida');
 SELECT is(ex5_helpers.fn_sigla('B2'), TRUE, 'Sigla válida');
