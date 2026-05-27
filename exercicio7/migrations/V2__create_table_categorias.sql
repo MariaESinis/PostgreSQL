62d697499d818e94c9dba8e1ad7e7903b92ffb78
Maria Eduarda Sinis <130075460+MariaESinis@users.noreply.github.com>
2026-05-27 17:10:16 -0300

    feat: create table categorias

---
+++ exercicio7/migrations/V2__create_table_categorias.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS ex7_cadastro.categorias(
+    id INTEGER GENERATED ALWAYS AS IDENTITY(
+        START WITH 0
+        INCREMENT BY 1
+        MINVALUE 0
+        MAXVALUE 1000000
+        CACHE 1
+        SEQUENCE NAME seq_categorias_id
+    ),
+
+    nome            VARCHAR(150)    NOT NULL,
+    descricao       VARCHAR(300)    NULL,
+
+    CONSTRAINT pk_categoria_id PRIMARY KEY (id)
+);
