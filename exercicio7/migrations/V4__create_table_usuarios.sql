24d1cc671133c23c036ee11b2e998bc238b0a4e4
Maria Eduarda Sinis <130075460+MariaESinis@users.noreply.github.com>
2026-05-29 13:44:20 -0300

    feat: create table usuarios

---
+++ exercicio7/migrations/V4__create_table_usuarios.sql
@@ -0,0 +1,28 @@
+CREATE TABLE IF NOT EXISTS ex7_cadastro.usuarios(
+    id  INTEGER GENERATED ALWAYS AS IDENTITY(
+        START WITH 0
+        INCREMENT BY 1
+        MINVALUE 0
+        MAXVALUE 100000000
+        CACHE 1
+        SEQUENCE NAME seq_usuario_id
+    ),
+
+    cpf                 VARCHAR(11)     NOT NULL,
+    email               VARCHAR(320)    NOT NULL,
+    telefone            VARCHAR(11),
+    nome                VARCHAR(150)    NOT NULL,
+    data_nascimento     TIMESTAMPTZ     NOT NULL,
+    idade               SMALLINT        NOT NULL,
+    status              ex7_cadastro.usuarios_status,
+    senha               VARCHAR(100)    NOT NULL,
+    created_date        TIMESTAMPTZ     NOT NULL,
+    updated_date        TIMESTAMPTZ     NOT NULL,
+
+    CONSTRAINT pk_cadastro_id PRIMARY KEY(id),
+
+    CONSTRAINT uq_cpf           UNIQUE(cpf),
+    CONSTRAINT uq_email         UNIQUE(email),
+    CONSTRAINT uq_telefone      UNIQUE(telefone)
+    
+);
