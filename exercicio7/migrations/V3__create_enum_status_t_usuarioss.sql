9d89fe9ceba3fe29561318436f88615f1a3960a5
Maria Eduarda Sinis <130075460+MariaESinis@users.noreply.github.com>
2026-05-29 13:44:06 -0300

    style: format for script

--- exercicio7/migrations/V3__create_enum_status_t_usuarioss.sql
+++ exercicio7/migrations/V3__create_enum_status_t_usuarioss.sql
@@ -1,10 +1,11 @@
 DO $$ 
-BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'usuarios_status') THEN
-        CREATE TYPE ex7_cadastro.usuarios_status AS ENUM (
-            'ativo',
-            'bloqueado',
-            'inativo'
-        );
-    END IF;
-END$$;
+    BEGIN
+        IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'usuarios_status') THEN
+            CREATE TYPE ex7_cadastro.usuarios_status AS ENUM (
+                'ativo',
+                'bloqueado',
+                'inativo'
+            );
+        END IF;
+    END
+$$;
