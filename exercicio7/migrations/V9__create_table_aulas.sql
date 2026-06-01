9c712c3456ef4c58aabe28fd2b271d9a1fb1bb71
Maria Eduarda Sinis <130075460+MariaESinis@users.noreply.github.com>
2026-06-01 11:15:54 -0300

    feat: create table aulas

---
+++ exercicio7/migrations/V9__create_table_aulas.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS ex7_academico.aulas(
+    id  INTEGER GENERATED ALWAYS AS IDENTITY(
+        START WITH 0
+        INCREMENT BY 1
+        MINVALUE 0
+        MAXVALUE 10000000
+        CACHE 1
+        SEQUENCE NAME seq_aulas_id
+    ),
+
+    url         VARCHAR(2500)                   NOT NULL,
+    titulo      VARCHAR(120)                    NOT NULL,
+    duracao     INTEGER                         NOT NULL,
+    ordem       SMALLINT                        NOT NULL,
+    status      ex7_cadastro.aulas_status       NOT NULL
+);
