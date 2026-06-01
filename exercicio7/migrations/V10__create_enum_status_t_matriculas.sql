9d7500f2f4d31aebe7285f80c6be845559cdb161
Maria Eduarda Sinis <130075460+MariaESinis@users.noreply.github.com>
2026-06-01 11:17:09 -0300

    feat: create enum for matriculas_status

---
+++ exercicio7/migrations/V10__create_enum_status_t_matriculas.sql
@@ -0,0 +1,11 @@
+DO $$ 
+    BEGIN
+        IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'matriculas_status') THEN
+            CREATE TYPE ex7_cadastro.matriculas_status AS ENUM (
+                'ativa',
+                'concluida',
+                'cancelada' 
+            );
+        END IF;
+    END
+$$;
