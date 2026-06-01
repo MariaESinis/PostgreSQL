c69f31fca84eceaa153b2882ce5896bcc48e0caf
Maria Eduarda Sinis <130075460+MariaESinis@users.noreply.github.com>
2026-06-01 14:40:14 -0300

    fix: name for schema

--- exercicio7/migrations/V8__create_enum_status_t_aulas.sql
+++ exercicio7/migrations/V8__create_enum_status_t_aulas.sql
@@ -1,7 +1,7 @@
 DO $$ 
     BEGIN
         IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'aulas_status') THEN
-            CREATE TYPE ex7_cadastro.aulas_status AS ENUM (
+            CREATE TYPE ex7_academico.aulas_status AS ENUM (
                 'publicada' 
                 'oculta' 
                 'removida' 
