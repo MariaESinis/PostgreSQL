2751ecd11c0d7ec19b1633aada03f4cbaeabd0d4
Maria Eduarda Sinis <130075460+MariaESinis@users.noreply.github.com>
2026-06-01 14:37:42 -0300

    fix: schema

--- exercicio7/migrations/V10__create_enum_status_t_matriculas.sql
+++ exercicio7/migrations/V10__create_enum_status_t_matriculas.sql
@@ -1,7 +1,7 @@
 DO $$ 
     BEGIN
         IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'matriculas_status') THEN
-            CREATE TYPE ex7_cadastro.matriculas_status AS ENUM (
+            CREATE TYPE ex7_academico.matriculas_status AS ENUM (
                 'ativa',
                 'concluida',
                 'cancelada' 
