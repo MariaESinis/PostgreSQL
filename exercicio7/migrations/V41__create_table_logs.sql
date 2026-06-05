e27604232f9c9506dabc5e991894d2a1ab2e6d24
mariaesinis <duda.sinis@gmail.com>
2026-06-05 18:04:42 -0300

    feat: add generated always

--- exercicio7/migrations/V41__create_table_logs.sql
+++ exercicio7/migrations/V41__create_table_logs.sql
@@ -1,5 +1,13 @@
-CREATE TABLE IF NOT EXISTS ex7_logs.l (
-    id                      BIGSERIAL, 
+CREATE TABLE IF NOT EXISTS ex7_logs.logs (
+    id  BIGINT GENERATED ALWAYS AS IDENTITY(
+        START WITH 1
+        INCREMENT BY 1
+        MINVALUE 1
+        MAXVALUE 1000000000
+        CACHE 1
+        SEQUENCE NAME seq_logs_id
+    ), 
+
     tipo_log                ex7_logs.log_tipo       NOT NULL,            
     usuario_id              INTEGER                 NULL,                                         
     dados_detalhados        JSONB                   NULL,                   
