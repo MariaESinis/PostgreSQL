91c820486406fcdf5b1f979d544ed280be35a856
mariaesinis <duda.sinis@gmail.com>
2026-06-08 19:12:39 -0300

    fix: function for email

--- exercicio7/migrations/V3__create_function_email.sql
+++ exercicio7/migrations/V3__create_function_email.sql
@@ -1,11 +1,12 @@
-CREATE OR REPLACE FUNCTION ex7_helpers.fn_email(IN p_email TEXT)
+CREATE OR REPLACE FUNCTION ex7_helpers.fn_email(p_email TEXT)
 RETURNS BOOLEAN
 LANGUAGE plpgsql
 AS $$
 BEGIN
-    RETURN(
-        p_email LIKE '_%@%._%' 
-        AND p_email ~* '^[A-Z0-9._%+-]{1,64}@[A-Z0-9.-]{1,255}\.[A-Z]{2,}$'
-    );
+    IF p_email IS NULL THEN
+        RETURN FALSE;
+    END IF;
+
+    RETURN p_email ~* '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$';
 END;
 $$;
