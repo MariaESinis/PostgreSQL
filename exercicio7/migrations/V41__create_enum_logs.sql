fa227e3cb0b9199b9ffade57d3072e3064f8fbf4
mariaesinis <duda.sinis@gmail.com>
2026-06-08 15:37:24 -0300

    fix: add idempotence

--- exercicio7/migrations/V41__create_enum_logs.sql
+++ exercicio7/migrations/V41__create_enum_logs.sql
@@ -1,10 +1,16 @@
-CREATE TYPE ex7_logs.log_tipo AS ENUM(
-    'LOGIN_REALIZADO',
-    'LOGIN_FALHOU',
-    'CURSO_CRIADO',
-    'MATRICULA_REALIZADA',
-    'PROVA_FINALIZADA',
-    'CERTIFICADO_EMITIDO',
-    'ALTERACAO_STATUS_CURSO',
-    'EXCLUSAO_LOGICA'
-);
+DO $$ 
+    BEGIN
+        IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'log_tipo') THEN
+            CREATE TYPE ex7_logs.log_tipo AS ENUM (
+                'LOGIN_REALIZADO',
+                'LOGIN_FALHOU',
+                'CURSO_CRIADO',
+                'MATRICULA_REALIZADA',
+                'PROVA_FINALIZADA',
+                'CERTIFICADO_EMITIDO',
+                'ALTERACAO_STATUS_CURSO',
+                'EXCLUSAO_LOGICA'            
+            );
+        END IF;
+    END
+$$;
