bc150561b6b7a260ebc9fc2f6a5c120f78197030
mariaesinis <duda.sinis@gmail.com>
2026-06-09 12:53:30 -0300

    test: remove info

--- tests/exercicio7/fn_email.test.sql
+++ tests/exercicio7/fn_email.test.sql
@@ -1,19 +1,11 @@
 BEGIN;
 
 SET search_path TO ex_pgtap;
-SELECT plan(4);
+SELECT plan(1);
 
 SELECT diag('Retorna TRUE se o email for válido');
 SELECT is(ex7_helpers.fn_email('maria.sinis@gmail.com'), TRUE, 'Email válido');
 
-SELECT diag('Retorna FALSE se o email nao tiver @');
-SELECT is(ex7_helpers.fn_email('maria.sinisgmail.com'), FALSE, 'Email inválido');
-
-SELECT diag('Retorna FALSE se o email nao tiver dominio');
-SELECT is(ex7_helpers.fn_email('maria.sinis@gmail'), FALSE, 'Email inválido');
-
-SELECT diag('Retorna FALSE se o email tiver simbolo');
-SELECT is(ex7_helpers.fn_email('m@ria.s!n!s@gmail.com'), FALSE, 'Email inválido');
 
 SELECT * FROM finish();
 
